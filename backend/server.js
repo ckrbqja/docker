84acecd0131c1f108e01d2e97a0c2447dd555c63
ckrbqja <67684166+ckrbqja@users.noreply.github.com>
2021-08-16 13:26:56 +0900

    docker setting

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = reqire("express")
+const bodyParser = require("body-parser")
+
+const db = require('./db');
+
+const app = express()
+
+//DB list 테이블에 있는 모든 데이터 보내주기
+app.get('/api/values', (req,res)=>{
+    //데이터베이스에서 모든 정보 가져오기
+    db.pool.query('SELECT * from lists',
+    (err,results, fileds)=>{
+        if(err)
+            return res.status(500).send(err)
+        else
+            return res.json({success:true, value:req.body.value})
+    });
+})
+
+app.post('/api/value',(req,res,next)=>{
+    db.pool.query(`insert into lists (value) VALUES("req.body.value")`,
+    (err,results, fileds)=>{
+        if(err)
+            return res.status(500).send(err)
+        else
+            return res.json({success:true, value:req.body.value})
+    })
+})
+
+//테이블 생성
+db.pool.query(`create table lists (
+    id integer auto_increment,
+    value text,
+    primary key (id)
+)`,
+(err,results, fileds)=>{
+    if(err)
+        return res.status(500).send(err)
+    else
+        return res.json({success:true, value:req.body.value})
+})
+
+
+app.listen(5000, () => {
+    console.log("5000 실행")
+})
