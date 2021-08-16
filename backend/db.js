80dd6ef334416e8402dfd1db59972456326368e7
ckrbqja <67684166+ckrbqja@users.noreply.github.com>
2021-08-16 16:59:21 +0900

    도커 완성

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
-const mysql = require("mysql");
+const mysql = require("mysql")
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: 'st123',
-    database: 'myapp'
+    host: "mysql",
+    user: "root",
+    password: "st123",
+    database: "myapp",
 })
 
 exports.pool = pool
